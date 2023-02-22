215bb35b02e655e8108d71b2da7cb35eb79742fe
carlos.costaaec <carlos.costa@aec.com.br>
2023-02-22 20:33:08 -0300

    Adiciona endpoints

--- src/ToDoAPI/Program.cs
+++ src/ToDoAPI/Program.cs
@@ -1,12 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+using ToDoaPI.Models;
 using ToDoApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("ToDoApiDb"));
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("ToDoApiDb"));
 
 var app = builder.Build();
 
@@ -18,9 +19,20 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/api/todoapi", () =>
+app.MapGet("api/todo", async (AppDbContext context) =>
 {
-    return Results.Ok();
+    var toDoitems = await context.ToDoItems.ToListAsync();
+    return Results.Ok(toDoitems);
+});
+
+app.MapPost("api/todo", async (AppDbContext context, ToDoItem toDoItem) =>
+{
+    if (toDoItem == null) return Results.BadRequest();
+
+    await context.ToDoItems.AddAsync(toDoItem);
+    await context.SaveChangesAsync();
+
+    return Results.Created($"api/todo/{toDoItem.Id}", toDoItem);
 });
 
 app.Run();
